1d07b6d3a8c3193df9515b171219101f14c8031c
Flame_BoY01 <anurag.kumar7227@gmail.com>
2026-01-16 22:24:03 +0530

    working on the nst features

--- backend/nst_engine.py
+++ backend/nst_engine.py
@@ -17,3 +17,21 @@ loader = transforms.Compose([
 unloader = transforms.ToPILImage()
 
 
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = nn.functional.mse_loss(input,self.target)
+        return input
+    
+
+# Gram Matrix to calculate the correlation btw the feture maps
+def gram_matrix(input):
+    a,b,c,d = input.size()
+    features = input.view(a*b,c*d)
+    g = torch.mm(features,features.t()) # matrix multiplicati0n
+    return g.div(a*b*c*d) # normalize
+
+    
